ee46785a2420102a1fcd30960a012aa9dfc23b63
glad-gradient <levitsky.serhii@gmaol.com>
2020-04-30 13:57:09 +0300

    Refactoring

--- efficientnet_classifier.py
+++ efficientnet_classifier.py
@@ -1,10 +1,8 @@
-from datetime import datetime
 import json
 import matplotlib.pyplot as plt
 import os
 
 import numpy as np
-from pandas import read_csv, merge, concat
 from skimage.io import imread
 from cv2 import resize
 
@@ -19,61 +17,28 @@ from keras.models import Model
 from efficientnet.keras import EfficientNetB0
 from efficientnet.keras import preprocess_input
 
+from utils import preprocess_data
 
-BATCH_SIZE = 4
-EPOCHS = 40
-STEPS_PER_EPOCH = 200
+
 RANDOM_SEED = 10000
-IMG_SIZE = 256
-LEARNING_RATE = 8e-5
-FREEZE = False
-
-PATH_CHECKPOINT = 'models'
 MODEL_NAME = 'efficientnet_classifier'
-LOAD_MODEL = None
 
 
-def rle_decode(mask_rle, shape=(768, 768)):
-    '''
-    mask_rle: run-length as string formated (start length)
-    shape: (height,width) of array to return
-    Returns numpy array, 1 - mask, 0 - background
-    '''
-
-    s = mask_rle.split()
-    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
-    starts -= 1
-    ends = starts + lengths
-    img = np.zeros(shape[0] * shape[1], dtype=np.uint8)
-    for lo, hi in zip(starts, ends):
-        img[lo:hi] = 1
-    return img.reshape(shape).T
-
-
-def masks_as_image(in_mask_list):
-    # Take the individual ship masks and create a single mask array for all ships
-    all_masks = np.zeros((768, 768), dtype=np.int16)
-    for mask in in_mask_list:
-        if isinstance(mask, str):
-            all_masks += rle_decode(mask)
-    return all_masks
-
-
-def make_image_generator(in_df, batch_size=BATCH_SIZE):
+def make_image_generator(in_df, _batch_size, _img_size, _train_image_dir):
     all_batches = list(in_df.groupby('ImageId'))
     out_rgb = []
     labels = []
     while True:
         np.random.shuffle(all_batches)
         for c_img_id, c_masks in all_batches:
-            rgb_path = os.path.join(train_image_dir, c_img_id)
+            rgb_path = os.path.join(_train_image_dir, c_img_id)
             _img = imread(rgb_path)
-            _img = resize(_img, (IMG_SIZE, IMG_SIZE))
+            _img = resize(_img, (_img_size, _img_size))
             _img = _img.astype(np.float32)
 
             out_rgb += [_img]
             labels.append(c_masks['has_ship'].iloc[0])
-            if len(out_rgb) >= batch_size:
+            if len(out_rgb) >= _batch_size:
                 yield np.stack(out_rgb, 0), np.array(labels)
                 out_rgb, labels = [], []
 
@@ -90,48 +55,31 @@ def create_aug_generator(in_gen, _image_generator, seed=RANDOM_SEED):
         yield next(sample)
 
 
-def preprocess_data(_train_image_dir, _exclude_list, n_train=0.8, balanced=False):
-    masks = read_csv(os.path.join(data_dir, 'train_ship_segmentations_v2.csv.zip'))
-
-    masks = masks[~masks['ImageId'].isin(_exclude_list)]
-
-    masks['ships'] = masks['EncodedPixels'].map(lambda row: 1 if isinstance(row, str) else 0)
-    unique_img_ids = masks.groupby('ImageId').agg({'ships': 'sum'}).reset_index()
-
-    unique_img_ids['has_ship'] = unique_img_ids['ships'].map(lambda x: 1.0 if x > 0 else 0.0)
-
-    n_train_split = int(n_train * len(unique_img_ids))
-
-    train_ids = unique_img_ids.iloc[:n_train_split]
-    valid_ids = unique_img_ids.iloc[n_train_split:]
-
-    if balanced:
-        ones = train_ids.loc[train_ids['has_ship'] == 1]
-        zeros = train_ids.loc[train_ids['has_ship'] == 0]
-        zeros = zeros.sample(len(ones))
-        train_ids = concat([ones, zeros])
-
-    masks.drop(['ships'], axis=1, inplace=True)
-
-    _train_df = merge(masks, train_ids)
-    _valid_df = merge(masks, valid_ids)
-
-    return _train_df, _valid_df
-
-
 if __name__ == '__main__':
-    exclude_list = ['6384c3e78.jpg', '13703f040.jpg', '14715c06d.jpg', '33e0ff2d5.jpg',
-                    '4d4e09f2a.jpg', '877691df8.jpg', '8b909bb20.jpg', 'a8d99130e.jpg',
-                    'ad55c3143.jpg', 'c8260c541.jpg', 'd6c7f17c7.jpg', 'dc3e7c901.jpg',
-                    'e44dffe88.jpg', 'ef87bad36.jpg', 'f083256d8.jpg', '66049e4ea.jpg']  # corrupted images
-
     with open('config.json', 'r') as file:
         configs = json.load(file)
+
+    model_configs = configs['CLASSIFIER']
+
+    balanced = model_configs['BALANCE_DATA']
+    batch_size = model_configs['BATCH_SIZE']
+    epochs = model_configs['EPOCHS']
+    steps_per_epoch = model_configs['STEPS_PER_EPOCH']
+    img_size = model_configs['IMG_SIZE']
+    learning_rate = model_configs['LEARNING_RATE']
+    path_checkpoint = model_configs['PATH_CHECKPOINT']
+    model_to_load = model_configs['LOAD_MODEL']
+    freeze = model_configs['FREEZE']
 
     data_dir = configs["DATA_DIRECTORY"]
     train_image_dir = os.path.join(data_dir, 'train_v2')
 
-    train_df, valid_df = preprocess_data(train_image_dir, exclude_list, n_train=0.85, balanced=True)
+    corrupted_images = configs['EXCLUDE_IMAGES']
+    train_df, valid_df = preprocess_data(
+        os.path.join(data_dir, 'train_ship_segmentations_v2.csv.zip'),
+        corrupted_images,
+        n_train=0.85,
+        balanced=balanced)
 
     data_generator_args = dict(
         rotation_range=15,
@@ -147,14 +95,20 @@ if __name__ == '__main__':
 
     data_generator = ImageDataGenerator(**data_generator_args)
 
-    train_generator = create_aug_generator(make_image_generator(train_df), data_generator)
-    valid_generator = create_aug_generator(make_image_generator(valid_df), data_generator)
+    train_generator = create_aug_generator(
+        make_image_generator(train_df, _batch_size=batch_size, _img_size=img_size, _train_image_dir=train_image_dir),
+        data_generator
+    )
+    valid_generator = create_aug_generator(
+        make_image_generator(valid_df, _batch_size=batch_size, _img_size=img_size, _train_image_dir=train_image_dir),
+        data_generator
+    )
 
     K.set_image_data_format('channels_last')
 
-    if LOAD_MODEL:
-        model = load_model(f'{PATH_CHECKPOINT}/{LOAD_MODEL}.hdf5')
-        MODEL_NAME = LOAD_MODEL
+    if model_to_load:
+        model = load_model(f'{path_checkpoint}/{model_to_load}.hdf5')
+        MODEL_NAME = model_to_load
 
         model_split = 'block2a_expand_conv'
 
@@ -173,7 +127,7 @@ if __name__ == '__main__':
 
         model = Model(inputs=base_model.input, outputs=predictions)
 
-        if FREEZE:
+        if freeze:
             for layer in base_model.layers:
                 layer.trainable = False
         else:
@@ -187,18 +141,18 @@ if __name__ == '__main__':
                 else:
                     layer.trainable = True
 
-    no_train_steps = min(STEPS_PER_EPOCH, len(train_df) // BATCH_SIZE)
-    no_valid_steps = min(STEPS_PER_EPOCH, len(valid_df) // BATCH_SIZE)
+    no_train_steps = min(steps_per_epoch, len(train_df) // batch_size)
+    no_valid_steps = min(steps_per_epoch, len(valid_df) // batch_size)
 
     callbacks = []
 
     model.compile(
-        optimizer=Adam(lr=LEARNING_RATE),
+        optimizer=Adam(lr=learning_rate),
         loss='binary_crossentropy',
         metrics=['accuracy']
     )
 
-    if LOAD_MODEL:
+    if model_to_load:
         FULL_MODEL_NAME = MODEL_NAME + '_retrained'
     else:
         FULL_MODEL_NAME = f'{MODEL_NAME}'
@@ -208,7 +162,7 @@ if __name__ == '__main__':
                                         mode='min',
                                         verbose=1)
 
-    checkpoint_callback = ModelCheckpoint(filepath=f'{PATH_CHECKPOINT}/{FULL_MODEL_NAME}.hdf5',
+    checkpoint_callback = ModelCheckpoint(filepath=f'{path_checkpoint}/{FULL_MODEL_NAME}.hdf5',
                                           monitor='val_loss',
                                           mode='min',
                                           verbose=1,
@@ -218,7 +172,7 @@ if __name__ == '__main__':
 
     history = model.fit_generator(
         train_generator,
-        epochs=EPOCHS,
+        epochs=epochs,
         steps_per_epoch=no_train_steps,
         validation_data=valid_generator,
         validation_steps=no_valid_steps,
